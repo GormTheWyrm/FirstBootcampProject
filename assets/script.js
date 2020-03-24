f9afa22bc7be04aabb45f16efe91940c8be9c334
jbrandona119 <56561899+jbrandona119@users.noreply.github.com>
2020-03-24 18:58:24 -0400

    added animation and markers to the JS file for webGL

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,17 @@
 
 
 function initialize() {
+
+          // Start a simple rotation animation
+          var before = null;
+          requestAnimationFrame(function animate(now) {
+              var c = earth.getPosition();
+              var elapsed = before? now - before: 0;
+              before = now;
+              earth.setCenter([c[0], c[1] + 0.1*(elapsed/30)]);
+              requestAnimationFrame(animate);
+          });
+          
   var options = { atmosphere: false, center: [37.540726, -77.436050], zoom: 5 };
   var earth = new WE.map('earth_div', options);
     WE.tileLayer('http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', {
@@ -14,7 +25,28 @@ function initialize() {
       maxZoom: 5,
       attribution: 'NASA'
     }).addTo(earth);
-}
+    
+    var marker = WE.marker([51.5, -0.09]).addTo(earth);
+        marker.bindPopup("<b>Hello world!</b><br>I am a popup.<br /><span style='font-size:10px;color:#999'>Tip: Another popup is hidden in Cairo..</span>", {maxWidth: 150, closeButton: true}).openPopup();
+
+        var marker2 = WE.marker([30.058056, 31.228889]).addTo(earth);
+        marker2.bindPopup("<b>Cairo</b><br>Yay, you found me!", {maxWidth: 120, closeButton: false});
+
+        var markerCustom = WE.marker([50, -9], '/img/logo-webglearth-white-100.png', 100, 24).addTo(earth);
+
+        earth.setView([51.505, 0], 6);
+
+                // Start a simple rotation animation
+        var before = null;
+        requestAnimationFrame(function animate(now) {
+            var c = earth.getPosition();
+            var elapsed = before? now - before: 0;
+            before = now;
+            earth.setCenter([c[0], c[1] + 0.1*(elapsed/30)]);
+            requestAnimationFrame(animate);
+        });
+      }
+
 initialize()
 
 $("#input-button").on("click", function(){
